3ee9a92f03ff3ebc003491b55726209a6cfb26af
Shefki Berisha <155109578+Shefki-Berisha@users.noreply.github.com>
2024-02-05 23:03:07 +0100

    Update main.py
    Made the code more easy to understand.

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import discord
 from discord.ext import commands
 
-bot = commands.Bot(command_prefix="!") #command_prefix can be channged to whatever value you want
+bot = commands.Bot(command_prefix="!") #command_prefix's value can be channged to whatever value you want
 
 @bot.event
 async def on_ready():
-    print("Ready!") #make the bot print Ready! in the console when the bot is ready
+    print("Ready!") #make the bot print "Ready!" in the console when the bot is ready
 
 #bot commands
 @bot.command()
@@ -17,6 +17,7 @@ async def command2(ctx):
 @bot.command()
 async def command3(ctx):
     await ctx.send("this is the 3rd command")
-#you can make as many commands as you want
+#you can make as many commands as you want, these are just examples
 
 bot.run("PLACE YOUR BOT TOKEN HERE")
+# to get your bot token, go to discord.dev and sign in if you havent already, go to applications and click on your bot, go to "Bot" and click "Reset Token" and copy the bot token
